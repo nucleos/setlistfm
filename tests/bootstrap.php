e17d754e562144bacc2fd8f300078d038654d214
core23 <mail@core23.de>
2017-03-25 11:09:57 +0100

    Update phpunit config

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -7,9 +7,18 @@
  * file that was distributed with this source code.
  */
 
+if (file_exists($file = __DIR__.'/autoload.php')) {
+    require_once $file;
+} elseif (file_exists($file = __DIR__.'/autoload.php.dist')) {
+    require_once $file;
+}
+
+// try to get Symfony's PHPunit Bridge
 $files = array_filter(array(
-    __DIR__.'../../vendor/autoload.php',
-    __DIR__.'../../../vendor/autoload.php',
+    __DIR__.'/../vendor/symfony/symfony/src/Symfony/Bridge/PhpUnit/bootstrap.php',
+    __DIR__.'/../vendor/symfony/phpunit-bridge/bootstrap.php',
+    __DIR__.'/../../../vendor/symfony/symfony/src/Symfony/Bridge/PhpUnit/bootstrap.php',
+    __DIR__.'/../../../vendor/symfony/phpunit-bridge/bootstrap.php',
 ), 'file_exists');
 if (count($files) > 0) {
     require_once current($files);
